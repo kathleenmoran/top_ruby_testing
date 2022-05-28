ca9bdfed7e6ea0987acd39b00ac46b39d0a98297
Kathleen Moran <kathleenmoran10@gmail.com>
2022-05-28 16:15:46 -0400

    Add solution to exercise 11

--- spec/11c_dog_spec.rb
+++ spec/11c_dog_spec.rb
@@ -35,17 +35,21 @@ end
 
 describe Dog do
   # Create a subject with your choice of dog name and optional breed/color.
+  subject(:fluffy) { described_class.new('Fluffy', 'Poodle', 'white') }
 
   # Write a test using the second shared_example to test that dog responds to
   # talk ('WOOF!').
-  context '' do
+  context 'dog responds to talk' do
+    include_examples 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not barking' do
+  it 'is not barking' do
+    expect(fluffy.barking?).to be false
   end
 
   # remove the 'x' before running this test
-  xit 'is sleeping' do
+  it 'is sleeping' do
+    expect(fluffy.sleeping?).to be true
   end
 end
